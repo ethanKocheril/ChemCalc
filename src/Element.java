14a28c62d8c6bb19c31506a908959c3ab52d340a
ethanKocheril <61663862+ethanKocheril@users.noreply.github.com>
2020-10-05 20:53:38 -0400

    Update Element.java
    Added moles/gram conversion methods

--- src/Element.java
+++ src/Element.java
@@ -1,4 +1,3 @@
-
 public class Element{
   private int atomicNumber;
   private double molarMass;
@@ -8,8 +7,17 @@ public class Element{
     this.atomicNumber = protonCount;
     this.molarMass = atomicMass;    
     this.name = substance;
-    }
+  }
+
+  public double gramsToMoles(double grams){
+    return (grams / this.molarMass);
+  }
+
+  public double molesToGrams(double moles){
+    return (moles * this.molarMass);
+  }
+
    public String toString(){
      return this.name + " has a molar mass of " + this.molarMass + " g, and an atomic number of " + this.atomicNumber;
-   } 
+   }
 }
